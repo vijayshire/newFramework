5806c0c8795f96f959764b1565737f6ebe62106a
vijayshire <vijayhire@rediffmail.com>
2020-06-20 17:58:53 +0530

    testdata

--- NewFramework/src/test/java/pkjUtilityClasses/TestData.java
+++ NewFramework/src/test/java/pkjUtilityClasses/TestData.java
@@ -14,6 +14,43 @@
 
 package pkjUtilityClasses;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class TestData {
 
+	public XSSFWorkbook workbook;
+	public XSSFSheet sheet;
+	public XSSFCell cell;
+	public XSSFRow row;
+
+	public Object[][] getData(String testCaseName) throws IOException {
+		FileInputStream file = new FileInputStream("C:\\Users\\vijay\\git\\newFramework\\NewFramework\\TestData.xlsx");
+		workbook = new XSSFWorkbook(file);
+		sheet = workbook.getSheet("sheet1");
+		String strTestCaseName = testCaseName;
+		int totalRows;
+		int testRows;
+		int totalColumn;
+
+		return null;
+
+	}
+
+	public int getTotalRows(XSSFSheet sheet) {
+		int totalRows = sheet.getLastRowNum();
+
+		for (int i = 0; i < totalRows; i++) {
+			if (sheet.getRow(i).getCell(0).getRawValue().isEmpty()) {
+
+			}
+		}
+		return 0;
+
+	}
 }
